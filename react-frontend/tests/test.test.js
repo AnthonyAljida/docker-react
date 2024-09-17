c1094a7fa5729879bb00155c0e7491f6be3eab0b
Anthony Aljida <anthonyaljida@icloud.com>
2024-09-17 09:50:46 -0400

    Second commit

---
+++ react-frontend/tests/test.test.js
@@ -0,0 +1,9 @@
+import { expect } from "vitest";
+import { it } from "vitest";
+import { describe } from "vitest";
+
+describe("An exceptionally useful test", () => {
+  it("should be 2", () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
